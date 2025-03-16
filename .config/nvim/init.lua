26ef6db8df2dd38ef6a9772c3fcb802ae7ac3501
c-auri <43008483+c-auri@users.noreply.github.com>
2025-03-16 23:02:20 +0100

    nvim: remove gitsigns plugin

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -139,20 +139,6 @@ require("lazy").setup({
 	-- "gc" to comment visual regions/lines
 	{ "numToStr/Comment.nvim", opts = {} },
 
-	-- Adds git related signs to the gutter, as well as utilities for managing changes, see `:help gitsigns`
-	{
-		"lewis6991/gitsigns.nvim",
-		opts = {
-			signs = {
-				add = { text = "+" },
-				change = { text = "~" },
-				delete = { text = "_" },
-				topdelete = { text = "‾" },
-				changedelete = { text = "~" },
-			},
-		},
-	},
-
 	-- Fuzzy Finder (files, lsp, etc)
 	{
 		"nvim-telescope/telescope.nvim",
