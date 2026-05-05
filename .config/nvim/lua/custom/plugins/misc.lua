cee264b7415112abec2e8d802a06c0bea3f918c2
c-auri <43008483+c-auri@users.noreply.github.com>
2026-05-05 23:27:40 +0200

    nvim: remove TODO comments

--- .config/nvim/lua/custom/plugins/misc.lua
+++ .config/nvim/lua/custom/plugins/misc.lua
@@ -6,13 +6,6 @@ return {
   { 'numToStr/Comment.nvim', opts = {} },
 
   {
-    'folke/todo-comments.nvim',
-    event = 'VimEnter',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    opts = { signs = false },
-  },
-
-  {
     '0xAdk/full_visual_line.nvim',
     keys = 'V',
     opts = {},
